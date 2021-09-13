ff79eb5e054fc60c028fb8b11d22c242c922a0ce
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-13 11:48:14 -0500

    Num Order

--- Chapter 3 RepoStarterCode/NumOrder.java
+++ Chapter 3 RepoStarterCode/NumOrder.java
@@ -4,8 +4,26 @@ public class NumOrder
 {
    public static void main(String[] args)
    {
+      int a,b,c;
+      int num1,num2,num3;
+     
       Scanner in = new Scanner(System.in);
       System.out.print("Enter three numbers: ");
+      
+      a = in.nextInt();
+      b = in.nextInt();
+      c = in.nextInt();
+      
+      if (a>b>c){
+          num1 = a;
+          num2 = b;
+          num3 = c;
+          
+          System.out.print(a,b,c);
+          
+        }
+      
+      
 
 
    }
