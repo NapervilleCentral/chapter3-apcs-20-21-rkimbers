196ecfe40362324f5540fc7616e442f16bcd0a68
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-15 12:59:38 -0500

    9_14 notes

---
+++ Chapter 3 RepoStarterCode/print9_14_21.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class print9_14_21 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class print9_14_21
+{
+    public static void main(String[]args){
+        System.out.println(11.0/9);
+
+    }
+}
