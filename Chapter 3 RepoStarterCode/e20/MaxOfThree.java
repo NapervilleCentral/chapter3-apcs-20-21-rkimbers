11edcca60e4591a2aca6578a07f1e95ee57d6fc9
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-16 12:19:42 -0500

    Update MaxOfThree.java

--- Chapter 3 RepoStarterCode/e20/MaxOfThree.java
+++ Chapter 3 RepoStarterCode/e20/MaxOfThree.java
@@ -1,3 +1,5 @@
+package e20;
+
 import java.util.Scanner;
 /**
  * Read three floating-point numbers and print
@@ -10,16 +12,22 @@ public class MaxOfThree
       Scanner in = new Scanner(System.in);
       System.out.print("Please enter three numbers: ");
       double largest = 0.0;
+      double a,b,c;
       
-      // Place your work here
-      . . .
+      a = in.nextDouble();
+      b = in.nextDouble();
+      c = in.nextDouble();
       
-      
-      
-      
-      
-      
+      if (a>b && a>c){ 
+          largest = a;
+        }
+      else if (b>a && b>c){ 
+          largest = b;
+        }
+      else if (c>a && c>b){
+          largest = c;
+        }
          
-      System.out.println("The largest number is " + largest + ".");
+      System.out.println("The largest number is " + largest + " yo.");
    }
 }
