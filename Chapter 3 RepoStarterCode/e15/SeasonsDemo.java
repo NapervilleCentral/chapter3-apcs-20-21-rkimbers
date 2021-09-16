cfb3104f255389a8b8101c1be9c28eabd57b896e
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-16 12:19:38 -0500

    Update SeasonsDemo.java

--- Chapter 3 RepoStarterCode/e15/SeasonsDemo.java
+++ Chapter 3 RepoStarterCode/e15/SeasonsDemo.java
@@ -1,3 +1,5 @@
+package e15;
+
 import java.util.Scanner;
 /**
    This program calculates the season for a given month and day.
@@ -13,8 +15,23 @@ public class SeasonsDemo
       int day = in.nextInt();
       //only used of writing class
       //Date date = new Date(month, day);
-
-      //. . .  your code here
-      System.out.println("Season: " + date.getSeason());
+      
+      if ((month >= 3 && month <= 6 && day >=20) || (month <= 6 && day < 20)){
+          
+          System.out.print("It is spring");
+        }
+      else if ((month >= 6 && month <= 9 && day >= 20) || (month <= 9 && day < 22)){
+          
+          System.out.print("It is summer");
+        }
+      else if ((month >= 9 && month <= 12 && day <= 22) || (month <= 12 && day < 21)){
+          
+          System.out.print("It is Fall");
+        }
+      else if ((month >= 12 && month <= 3 && day >= 21) || (month <= 3 && day < 20)){
+          
+          System.out.print("It is Winter");
+        }  
+      //System.out.println("Season: " + date.getSeason());
    }
 }
