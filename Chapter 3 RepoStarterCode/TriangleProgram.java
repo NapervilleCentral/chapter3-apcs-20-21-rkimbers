96319147a528c0c3c2491025cb93237d1f359f08
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-20 11:57:31 -0500

    Create TriangleProgram.java

---
+++ Chapter 3 RepoStarterCode/TriangleProgram.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class TriangleProgram here.
+ *
+ * Ryan Kimberley
+ * APCS Per 5 Triangle Program
+ */
+
+import java.util.Scanner;
+public class TriangleProgram
+{
+    public static void main(String[]args){
+        
+        Scanner in = new Scanner(System.in);
+        int a1, a2, a3, s1, s2, s3;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
