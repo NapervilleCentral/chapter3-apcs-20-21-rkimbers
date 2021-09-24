608a4ba38bdeec0a0f2be84f4cf1777707aaa9aa
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-24 12:20:38 -0500

    Planet Program

---
+++ Chapter 3 RepoStarterCode/PlanetProgram.java
@@ -0,0 +1,76 @@
+
+/**
+ *
+ *Ryan Kimberley
+ *Planet Program
+ *APCS Per 5
+ */
+import java.util.Scanner;
+
+public class PlanetProgram
+{
+    public static void main(String[]args){
+            char choice;
+            double weight;
+            double nw;
+            
+            Scanner Keyboard = new Scanner(System.in);
+            System.out.print("What planet do you want to weigh yourself on? ");
+            System.out.print("m) Mercury\n");
+            System.out.print("v) Venus\n");
+            System.out.print("e) Earth");
+            System.out.print("M) Mars\n");
+            System.out.print("j) Jupiter\n");
+            System.out.print("s) Saturn\n");
+            System.out.print("u) Uranus\n");
+            System.out.print("n) Neptune\n");
+
+            choice = Keyboard.next().charAt(0);
+            
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter in your weight: ");
+            weight = in.nextDouble();
+            
+            
+            
+             switch (choice){
+                case 'm':
+                    nw = weight*0.38;
+                    break;
+                case 'v':
+                    nw = weight*0.91;
+                    break;
+                case 'e':
+                    nw = weight;
+                    break;
+                case 'M':
+                    nw = weight*0.38;
+                    break;
+                case 'j':
+                    nw = weight*2.34;
+                    break;
+                case 's':
+                    nw = weight*1.06;
+                    break;
+                case 'u':
+                    nw = weight*0.92;
+                    break;
+                case 'n':
+                    nw = weight*1.19;
+                    break;
+                default:
+                    System.out.print("Invalid input");
+                    
+                
+                
+                
+                
+                
+                
+             }
+        
+             System.out.print("Your weight is "+nw+"lbs");
+        
+        
+    }
+}
