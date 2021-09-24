d1eb8bcff4c5c00b0579025055c833957150cf25
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-24 12:20:26 -0500

    Minivan Program

---
+++ Chapter 3 RepoStarterCode/MinivanProgram.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class MinivanProgram here.
+ *
+ *Ryan Kimberley
+ */
+import java.util.Scanner;
+public class MinivanProgram
+{
+    public static void main(String[]args){
+        int dl, dr, cl, mu, il, ir, ol, or;
+        String gear;
+        
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter in status code: ");
+        dl = in.nextInt();
+        dr = in.nextInt();
+        cl = in.nextInt();
+        mu = in.nextInt();
+        il = in.nextInt();
+        ir = in.nextInt();
+        ol = in.nextInt();
+        or = in.nextInt();
+        gear = in.nextLine();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
