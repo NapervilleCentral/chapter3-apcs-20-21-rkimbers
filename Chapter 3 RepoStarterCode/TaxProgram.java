b4f4aefb9cdac4d588cd22063905e09ce6e68218
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-27 11:23:40 -0500

    Tax Program

--- Chapter 3 RepoStarterCode/TaxProgram.java
+++ Chapter 3 RepoStarterCode/TaxProgram.java
@@ -11,17 +11,32 @@ import java.text.DecimalFormat;
 public class TaxProgram
 {
     public static void main(String[]args){
-        int tax;
-        
+        double tax;
+        double total = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter in your income: $");
-        tax = in.nextInt();
+        tax = in.nextDouble();
         
+        if (tax > 500000){
+            total = tax*0.06;
+        }
+        if (tax > 250000){
+            total = tax*0.05;
+        }
+        if (tax > 100000){
+            total = tax*0.04;
+        }
+        if (tax > 75000){
+            total = tax*0.03;
+        }
+        if (tax > 50000){
+            total = tax*0.02;
+        }
+        if (tax > 0){
+            total = tax*0.01;
+        }
         
-        
-        
-        
-        
+        System.out.print("You're taxed $"+total);
         
         
         
