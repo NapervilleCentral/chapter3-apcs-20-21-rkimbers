a0036dd70460f3ea063342e2e2048600625d421b
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-27 11:13:00 -0500

    Tax Program

---
+++ Chapter 3 RepoStarterCode/TaxProgram.java
@@ -0,0 +1,56 @@
+
+/**
+ * 
+ * Tax Program
+ * Ryan Kimberley
+ * APCS Per 5
+ */
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TaxProgram
+{
+    public static void main(String[]args){
+        int tax;
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter in your income: $");
+        tax = in.nextInt();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
