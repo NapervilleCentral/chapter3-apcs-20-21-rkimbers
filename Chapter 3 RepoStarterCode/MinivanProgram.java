c2a2129c33929571cbd08c509d7539d1c6db9845
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-09-29 12:20:57 -0500

    MinivanProgram

--- Chapter 3 RepoStarterCode/MinivanProgram.java
+++ Chapter 3 RepoStarterCode/MinivanProgram.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class MinivanProgram here.
  *
@@ -8,35 +7,43 @@ import java.util.Scanner;
 public class MinivanProgram
 {
     public static void main(String[]args){
-        int dl, dr, cl, mu, il, ir, ol, or;
+        boolean DL, DR, CL, MU, IL, IR, OL, OR;
         String gear;
-        
+        String p = new String("P");
         
         Scanner in = new Scanner(System.in);
         System.out.print("Enter in status code: ");
-        dl = in.nextInt();
-        dr = in.nextInt();
-        cl = in.nextInt();
-        mu = in.nextInt();
-        il = in.nextInt();
-        ir = in.nextInt();
-        ol = in.nextInt();
-        or = in.nextInt();
+        DL = in.nextInt();
+        DR = in.nextInt();
+        CL = in.nextInt();
+        MU = in.nextInt();
+        IL = in.nextInt();
+        IR = in.nextInt();
+        OL = in.nextInt();
+        OR = in.nextInt();
         gear = in.nextLine();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+      
+
+      if (gear.equals(p) && MU = 1){
+        if (CL = 1){
+          System.out.println("Both doors stay closed");
+        }
+        else if (CL = 0){
+          if (IL = 0 || OL = 0){
+            if (IR = 0 || OR = 0){
+              System.out.println("Both doors are opened");
+            }
+            else{
+              System.out.printlm("Left door opened");
+            }
+          }
+          else if (IR = 0 || OR = 0){
+            System.out.println("Right door opened");
+          }
+        }
+
+      }
+
+
     }
 }
