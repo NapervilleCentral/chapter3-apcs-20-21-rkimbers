e852e58df9a0cdc8cc4596f6cbe6057eac9c60f0
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-04 07:37:09 -0500

    MinivanProgram

--- Chapter 3 RepoStarterCode/MinivanProgram.java
+++ Chapter 3 RepoStarterCode/MinivanProgram.java
@@ -10,7 +10,6 @@ public class MinivanProgram
     public static void main(String[]args){
         int DL, DR, CL, MU, IL, IR, OL, OR;
         String gear;
-        String p = new String("P");
         
         Scanner in = new Scanner(System.in);
         System.out.print("Enter in status code: ");
@@ -26,26 +25,29 @@ public class MinivanProgram
         gear = in.nextLine();
       
 
-      if (gear.equals(p) && MU == 1){
+      if (gear.equals("P") && MU == 1){
         if (CL == 1){
-          if(OR == 1 && OL == 1){
+          if ((OR == 1 || DR ==1) && (OL == 1 || DL == 1)){
               System.out.print("Both doors opened");
             }
-          else if ((OR == 0 || OL == 0) && (IL == 0 || IR == 0)){
-              System.out.print("Both doors opened");
+          else if(OR == 1 || DR == 1){
+              System.out.print("Right door opened");
             }
-          else if(IL == 1 && IR == 0){
+          else if (OL == 1 || DL == 1){
+              System.out.print("Left doors opened");
+            }
+          
+          /*else if(IL == 1 && IR == 0){
               System.out.print("Right door opened");
             }
           else if (IR == 1 && IL == 0){
               System.out.print("Left door opened");
             }
-            
-          else{  
-          System.out.println("Both doors stay closed");
+            */
+
         }
         }
-        else if (CL == 0){
+        else if (gear.equals("P") && CL == 0){
           if (IL == 0 || OL == 0){
             if (IR == 1 || OR == 1){
               System.out.println("Both doors are opened");
@@ -57,14 +59,13 @@ public class MinivanProgram
           else if (IR != 1 && OR == 1){
             System.out.println("Right door opened");
           }
-        }
-        else{
+        
+            else{
             System.out.print("Both doors stay closed");
         }
-      }
-      else{
-         System.out.print("Both doors closed"); 
-        }
-
     }
+    else{
+       System.out.print("Both doors closed"); 
+        }
+}
 }
