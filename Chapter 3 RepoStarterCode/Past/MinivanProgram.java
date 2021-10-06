80856a77e6a686537d548a21c2591a43f6055057
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-06 12:11:29 -0500

    MinivanProgram

--- Chapter 3 RepoStarterCode/Past/MinivanProgram.java
+++ Chapter 3 RepoStarterCode/Past/MinivanProgram.java
@@ -3,12 +3,13 @@
  *
  *Ryan Kimberley
  *APCS Per 5 
- *Unfinished 10/4/2021 7:38 am
+ *Finished! Ready to check 10/4 7pm
  */
 import java.util.Scanner;
 public class MinivanProgram
 {
     public static void main(String[]args){
+        //creating variables and string
         int DL, DR, CL, MU, IL, IR, OL, OR;
         String gear;
         
@@ -24,8 +25,10 @@ public class MinivanProgram
         OR = in.nextInt();
         in.nextLine();
         gear = in.nextLine();
-      
+      //setting variables equal to input given
 
+      
+      //sorting through possibilities using if statements
     if (gear.equals("P") && MU == 1){
         if (CL == 1){
           if ((OR == 1 || DR ==1) && (OL == 1 || DL == 1)){
@@ -38,9 +41,9 @@ public class MinivanProgram
               System.out.print("Left doors opened");
             }
            }
-    }
+    
         else if (CL == 0){
-          if (IL == 0 || OL == 0 || DL == 1){
+          if (IL == 1 || OL == 1 || DL == 1){
             if (IR == 1 || OR == 1 || DR == 1){
               System.out.println("Both doors are opened");
             }
@@ -56,8 +59,11 @@ public class MinivanProgram
             System.out.print("Both doors stay closed");
         }
     }
+    } //else incase initial if is false
     else{
        System.out.print("Both doors closed"); 
         }
+
+
 }
 }
