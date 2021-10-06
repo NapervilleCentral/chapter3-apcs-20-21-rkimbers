a777d97b6201f78e200bdb289a65d4faef98630d
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-06 12:49:49 -0500

    3B

--- Chapter3B Starter Code/e02/PowerOf2.java
+++ Chapter3B Starter Code/e02/PowerOf2.java
@@ -1,8 +1,23 @@
+package e02;
+
+import java.lang.Math;
+
 public class PowerOf2
 {
    public static void main(String[] args)
    {
       // The powers of 2 from 2^0 to 2^20
+      int  num = 2;
+      int total = 0;
+      
+   for(int i = 0; i < 21; i++)
+ {    
 
+       System.out.println(Math.pow(num,i));
+       total += Math.pow(num,i);
+       
    }
+   System.out.print()
+}
+
 }
--- Chapter3B Starter Code/e02/SumOfOddDigits.java
+++ Chapter3B Starter Code/e02/SumOfOddDigits.java
@@ -1,14 +1,27 @@
+package e02;
+
 import java.util.Scanner;
 
 public class SumOfOddDigits
 {
    public static void main(String[] args)
    {
-      Scanner in = new Scanner(System.in);
-
+      String word = new String("A");
+      
+      while (!word.equals("Q")){
+          
+          Scanner in = new Scanner(System.in);
+          
+          
+          
+          
+          
+        }
+      
       // Get the input
 
       // The sum of odd digits of the input
+      // enter Q to stop
 
          // Only add digit if it is odd
 
