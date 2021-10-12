f41f77430d835d1b5794ac9cae38e84b236595a5
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-12 11:25:01 -0500

    SumOfOddDigits

--- Chapter3B Starter Code/e02/SumOfOddDigits.java
+++ Chapter3B Starter Code/e02/SumOfOddDigits.java
@@ -7,17 +7,28 @@ public class SumOfOddDigits
    public static void main(String[] args)
    {
       String word = new String("A");
+      int num = 0;
+      int total = 0;
       
       while (!word.equals("Q")){
           
           Scanner in = new Scanner(System.in);
+          System.out.println("Enter in a number ");
+          num = in.nextInt();
           
+          if (num%2 != 0){
+             total += num;
+              
+            }
+         
           
+          Scanner in1 = new Scanner(System.in);
+          System.out.println("Enter 'Q' to stop, A to continue ");
+          word = in1.nextLine();
           
-          
-          
-        }
+       }
       
+      System.out.println("Total sum of odd numbers, "+total);
       // Get the input
 
       // The sum of odd digits of the input
