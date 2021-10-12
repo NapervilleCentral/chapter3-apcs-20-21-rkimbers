58cb72ec216382f5d64a06b5082d5bc649b42f2e
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-12 12:08:29 -0500

    LargestInput

--- Chapter3B Starter Code/e03/LargestInput.java
+++ Chapter3B Starter Code/e03/LargestInput.java
@@ -6,15 +6,39 @@ public class LargestInput
 {
    public static void main(String[] args)
    {
-      Scanner in = new Scanner(System.in);
 
+    
+      
+      String start = new String("Q");
       System.out.print("Enter a series of numbers, then type Q to process: ");
-      //int largest  = . . .;
-      //int smallest = . . .;
+      
+      Scanner in = new Scanner(System.in);
+      String number = in.next();
+      int num = Integer.parseInt(number);
+      int largest = Integer.parseInt(number);
+      int smallest = Integer.parseInt(number);    
+        
+      while (!number.equals("Q")){
+        if (largest >= smallest){
+             System.out.print("Largest: " + largest + "\n" + "Smallest: " + smallest);
+            
+            
+        }
+        
+        
+        }
+      
 
+
+            
+      
+      
+          
       // Loop on input and check for a new min or max
 
       // Output the results
-      System.out.print("Largest: " + largest + "\n" + "Smallest: " + smallest);
-   }
+      
+   
+   System.out.print("Largest: " + largest + "\n" + "Smallest: " + smallest);
+}
 }
