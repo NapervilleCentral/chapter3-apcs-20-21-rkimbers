3489f6efa4a0ffe84c33482a9a92514a9e44768e
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-14 11:42:38 -0500

    LargestInput

--- Chapter3B Starter Code/e03/LargestInput.java
+++ Chapter3B Starter Code/e03/LargestInput.java
@@ -7,38 +7,53 @@ public class LargestInput
    public static void main(String[] args)
    {
 
-    
-      
-      String start = new String("Q");
-      System.out.print("Enter a series of numbers, then type Q to process: ");
-      
+   
+     
+     
+      System.out.print("Enter a series of numbers, and type Q to process: ");
+     
       Scanner in = new Scanner(System.in);
-      String number = in.next();
-      int num = Integer.parseInt(number);
-      int largest = Integer.parseInt(number);
-      int smallest = Integer.parseInt(number);    
-        
-      while (!number.equals("Q")){
-        if (largest >= smallest){
-             System.out.print("Largest: " + largest + "\n" + "Smallest: " + smallest);
-            
-            
+   
+       
+      String num; // returns num as string
+      int num1 = 0; // integer defined
+     
+      num = in.next();// read first word of the input string
+      num1 = Integer.parseInt(num);// converts num into integer
+     
+      int largest = num1; //  assigns first number as largest
+      int smallest = num1;// assigns first number as smallest
+       
+     while (true) // to read each word until the end of string
+         {
+           num = in.next();// reads next word of the input string
+           
+           if (num.equals("Q")) //check if end of string
+           {
+               break;
+           }
+           num1 = Integer.parseInt(num);// convert string into integer
+           
+           if (num1 >= largest) // checks for the largest number and assigns as largest
+           {
+               largest = num1;
+           }
+           if (num1 <= smallest)    // checks for the smaller number and assigns as smallest
+           {
+               smallest = num1;
+           }    
         }
-        
-        
-        }
-      
+       
+      System.out.print("Largest: " + largest + "\n" + "Smallest: " + smallest); // Output
+    }
+     
 
-
-            
-      
-      
-          
+}
+           
+     
+     
+         
       // Loop on input and check for a new min or max
 
       // Output the results
-      
-   
-   System.out.print("Largest: " + largest + "\n" + "Smallest: " + smallest);
-}
-}
+     
