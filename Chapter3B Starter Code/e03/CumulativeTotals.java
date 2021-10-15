d7c00dbc1e8145c99f24ed2ef7ea04e9e1420a25
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-15 11:32:00 -0500

    CumulativeTotals

--- Chapter3B Starter Code/e03/CumulativeTotals.java
+++ Chapter3B Starter Code/e03/CumulativeTotals.java
@@ -1,24 +1,31 @@
 package e03;
-
 import java.util.Scanner;
 
 public class CumulativeTotals
 {
    public static void main(String[] args)
    {
+      double number = 0.0;  //initializing doubles
       double sum = 0.0;
-      Scanner in = new Scanner(System.in);
+      
+      Scanner in = new Scanner(System.in); //scanner for inputting numbers
+      System.out.println("Enter in a series of numbers: "); //prompt for user input
+      String num = in.nextLine(); //gathering input from user
+      
+      
+      
+      in = new Scanner(num); //setting in equal to scannner input
+      while (in.hasNextDouble()){ //while the input has a next double, it continues
+        number = in.nextDouble(); // setting number equal to next double within the input
+        sum += number; //adding up the sum
+        
+        
+          
+        
+          
+ }
 
-      while (){
-          
-          
-          
-          
-          
-          
-          
-          
-        }
+System.out.print("Sum of input: "+sum);
       
       // 1,2,3,4,5 - display the total
       //more numbers? y
