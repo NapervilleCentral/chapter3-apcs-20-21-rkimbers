502fe6df9bf891f4b6bb27da34f24bc342a28823
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-17 17:38:17 -0500

    Test

--- Chapter3B Starter Code/test.java
+++ Chapter3B Starter Code/test.java
@@ -8,27 +8,5 @@ import java.util.Random;
  */
 public class test
 {
-    public static void main(String[] args){
-    Random rand = new Random();
-        
-    for(int i = 1; i< 25; i = i+5){
-     if (i%5 == 0)
-        {
-        System.out.print(i);
-        }
-        
-        
-        
-        
-    }
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    }
 }
+
