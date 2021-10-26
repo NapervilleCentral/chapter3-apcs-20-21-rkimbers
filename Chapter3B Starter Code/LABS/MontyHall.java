2616a890c3133c42a02a52c018d052c4c7cd1369
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-26 12:21:33 -0500

    MontyHall

--- Chapter3B Starter Code/LABS/MontyHall.java
+++ Chapter3B Starter Code/LABS/MontyHall.java
@@ -1,9 +1,6 @@
 package LABS;
-
 import java.lang.Math;
-
 /**
- * 
  * Ryan Kimberley
  * APCS Per 5
  * Monty Hall Paradox
@@ -13,7 +10,6 @@ public class MontyHall
     public static void main(String[]args){
         int s1 = 0;
         int s2 = 0;
-
         for (int i = 0; i < 1000; i++){
             
             int pick = (int)(Math.random()*3);
@@ -22,30 +18,17 @@ public class MontyHall
             int Switch = (int)(Math.random()*3);
             
             while (remove == pick || remove == winner){
-                remove = (int)(Math.random()*3);    
-            }
-            
-            
-            
-            while (Switch == pick || Switch == remove)
-            {
-                Switch = (int)(Math.random()*3);
-            }
-            //strategy 1
-            
-            if (Switch == winner){
+                remove = (int)(Math.random()*3);   }
+            while (Switch == pick || Switch == remove){
+                Switch = (int)(Math.random()*3);   }
+            if (Switch == winner){   //strategy 1
                 s1 += 1;
             }
-            
-            //strategy 2
-            
-            if (pick == winner){
+            else if (pick == winner){     //strategy 2
                 s2 += 1;
             }
-            
         }
         System.out.println("Strategy 1 results "+s1);
-        System.out.println("Strategy 2 results "+s2);
-        
+        System.out.println("Strategy 2 results "+s2); 
     }
 }
