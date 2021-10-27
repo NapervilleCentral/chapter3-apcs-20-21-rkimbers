95eb94e29927602f0f6cc6f63d232d74ada30f51
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-10-27 12:43:44 -0500

    SpeechProgram

---
+++ Chapter3B Starter Code/LABS/SpeechProgram.java
@@ -0,0 +1,24 @@
+package LABS;
+
+import java.io.*;
+import java.util.Scanner;
+
+
+/**
+ * Write a description of class Speeches here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SpeechProgram
+{
+    public static void main(String[]args) throws IOException{
+        
+        //C:\Users\rkimberley\Desktop\APCS Github\chapter3-apcs-20-21-rkimbers\Chapter3B Starter Code\speeches
+        
+        Scanner ScanFile = new Scanner(new File("speeches/gwbush.txt"));
+        
+        
+        
+    }
+}
