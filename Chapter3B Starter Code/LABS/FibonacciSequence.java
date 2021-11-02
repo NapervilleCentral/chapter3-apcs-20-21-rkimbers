da11068538d427df00d261b510c3c4151a50a107
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-11-02 12:20:31 -0500

    FibonacciSequence

--- Chapter3B Starter Code/LABS/FibonacciSequence.java
+++ Chapter3B Starter Code/LABS/FibonacciSequence.java
@@ -16,32 +16,32 @@ public class FibonacciSequence {
 
     public static void main(String[] args){
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);   //gathering input
         System.out.println("Enter in value you want to receive: ");
-        int pos = in.nextInt();
-        in.close();
+        int pos = in.nextInt(); //setting int equal to user input
+        in.close(); //closing scanner, ide making me do this
 
-        int num = 1;
+        int num = 1; //initialzing variables
         int add = 0;
         int v1 = 0;
         int v2 = 0;
 
-        if (pos >= 1){
+        if (pos >= 1){ //checking what value  intpos should print
             num = 0;
             System.out.print(num+" ");
         }
 
-        if (pos >= 2){
+        if (pos >= 2){ //printing if int pos is greater than 2
             num = 1;
             System.out.print(num);
         }
 
-        if (pos >= 3) {
-            for (int i = 0; i < (pos-2); i++) {
-                v1 = v2;
-                v2 = num;
-                num = (v1+v2);
-                System.out.print(" "+num);
+        if (pos >= 3) { //checking if int pos is greater than 3
+            for (int i = 0; i < (pos-2); i++) { //for loop that runs for the value of pos
+                v1 = v2; 
+                v2 = num; //changing values of first and second in sequence, for the next time loop runs
+                num = (v1+v2); //re-defining num
+                System.out.print(" "+num); //outputting num
             }
         }
         
