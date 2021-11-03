61f6c01ded5add4ad6ab0645c314a45eeae7fce3
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-11-03 12:42:16 -0500

    FourLeafRose

--- Chapter3B Starter Code/LABS/FourLeafRose.java
+++ Chapter3B Starter Code/LABS/FourLeafRose.java
@@ -1,21 +1,34 @@
 package LABS;
 
+import java.util.*;
+import TurtleGraphics.StandardPen;
 
-/**
- * Write a description of class FourLeafRose here.
- *
- * @author (your name)
- * @version (a version number or a date)
+/*
+ * Ryan Kimberley
+ * APCS Per 5
+ * Four Leaf Rose program
  */
+
 public class FourLeafRose
 {
-    public static void main(String[]args){
+    public static void main(String[] args)
+    {
+        double theta, r, xPos, yPos; //initializing doubles
+        
+        StandardPen pen = new StandardPen(); //initializing pen
+        
+        pen.home(); //setting pen at origin 
         
         
-        
-        
-        
-        
-        
+        for (int i = 0; i < 101; i++){ //for loop designed to run 100 times
+            
+            theta = i * 2 * Math.PI / 100; //setting value for theta
+            r = Math.cos(2*theta);  //formula for 'r'
+            xPos = 100 * r * Math.cos(theta);  //calculating new x,y coordinates
+            yPos = 100 * r * Math.sin(theta);
+            
+            pen.move(xPos,yPos); //moving the pen
+            }
+        }
     }
-}
+
