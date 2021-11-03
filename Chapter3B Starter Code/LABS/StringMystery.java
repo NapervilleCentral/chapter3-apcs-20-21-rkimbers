35ae3fd493d787ef4f56898e1d1402a0ee8d985d
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-11-03 12:42:27 -0500

    StringMystery

--- Chapter3B Starter Code/LABS/StringMystery.java
+++ Chapter3B Starter Code/LABS/StringMystery.java
@@ -1,15 +1,33 @@
 package LABS;
 
-/**
- * Ryan Kimberley
- * APCS Per 5
- * String Mystery Program
- */
-public class StringMystery
-{
+import java.util.Scanner;
+
+
+
+public class StringMystery {
+
     public static void main(String[]args){
-        
-        
-        
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter in a mystery string");
+    String inputMystery = in.nextLine();
+    int currentDigit = 0;
+    int lastDigit = 0;
+
+    lastDigit = inputMystery.charAt(inputMystery.length()-1);
+    //System.out.println(inputMystery.length() -1 );
+    System.out.println(lastDigit);
+
+
+    for (int i = 0; i < inputMystery.length() - 1; i++) {
+        currentDigit = inputMystery.charAt(i);
+        System.out.print(" "+currentDigit);
+
+
+
+
+
+
+    }
     }
 }
