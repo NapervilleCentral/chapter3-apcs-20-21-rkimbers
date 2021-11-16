073065c18826757435ede3da0c7507cf2a262550
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-11-15 20:06:34 -0600

    StringMystery

--- Chapter3B Starter Code/LABS/StringMystery.java
+++ Chapter3B Starter Code/LABS/StringMystery.java
@@ -1,6 +1,8 @@
 package LABS;
 
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 /*
  * Ryan Kimberley
@@ -9,25 +11,49 @@ import java.util.Scanner;
  */
 public class StringMystery {
 
-    public static void main(String[]args){
+    public static void main(String[] args)throws IOException {
 
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter in a mystery string");
-    String inputMystery = in.nextLine();
-    int currentDigit = 0;
-    int lastDigit = 0;
-    int mysteryLength = 0;
-    
-    mysteryLength = inputMystery.length() - 1;
-    lastDigit = inputMystery.charAt(mysteryLength);
-    System.out.println(lastDigit);
+        Scanner in = new Scanner(new File("LABS/String Mystery input.txt"));
+        String stringMystery = in.nextLine();
 
+        int currentDigit = Integer.parseInt(stringMystery.substring(0, 1));
+        int lastDigit = Integer.parseInt(stringMystery.substring(2195, 2196));
+        int secondDigit, count, mysteryLength;
+        
+        count = 0;
 
-  //  for (int i = 0; i < mysteryLength; i++) {
- //       currentDigit = inputMystery.charAt(i);
- //       System.out.print(" "+currentDigit);
+        mysteryLength = stringMystery.length() - 1;
 
 
-//    }
-    }
+
+        for (int i = 0; i < mysteryLength; i++) {
+            currentDigit = Integer.parseInt(stringMystery.substring(i, i+1));
+            secondDigit = Integer.parseInt(stringMystery.substring(i+1, i+2));
+
+
+            if (currentDigit == secondDigit) {
+                count += currentDigit - '0';
+            }
+            if (currentDigit == lastDigit){
+                count += currentDigit - '0';
+
+            }
+        }
+        System.out.print("Count: "+count);
+        
+        
+        mysteryLength = (stringMystery.length() - 1)/2;
+        
+        for (int i = 0; i < mysteryLength; i++){
+            
+            currentDigit = Integer.parseInt(stringMystery.substring(i, i+1));
+            
+            if (currentDigit == secondDigit) {
+                count += currentDigit - '0';
+            }
+            
+        }
+        System.out.print("Count: "+count);
+        }   
 }
+
