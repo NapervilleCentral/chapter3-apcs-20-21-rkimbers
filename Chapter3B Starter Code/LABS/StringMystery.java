b1034123d898d4bb70a23ba001de5bbb90e2aa22
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-11-16 18:14:54 -0600

    StringMystery

--- Chapter3B Starter Code/LABS/StringMystery.java
+++ Chapter3B Starter Code/LABS/StringMystery.java
@@ -1,6 +1,5 @@
 package LABS;
 
-import java.util.Scanner;
 import java.util.*;
 import java.io.*;
 
@@ -8,41 +7,41 @@ import java.io.*;
  * Ryan Kimberley
  * APCS Per 5
  * String Mystery program
+ * 
+ * 1341
+ * 2036
  */
 public class StringMystery {
 
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException { //throwing an IO exception
 
-        Scanner in = new Scanner(new File("LABS/String Mystery input.txt"));
+        Scanner in = new Scanner(new File("LABS/String Mystery input.txt")); 
         String stringMystery = in.nextLine();
 
-        int currentDigit = Integer.parseInt(stringMystery.substring(0, 1));
-        int lastDigit = Integer.parseInt(stringMystery.substring(2195, 2196));
-        int secondDigit, count, mysteryLength;
+        int currentDigit = Integer.parseInt(stringMystery.substring(0, 1));  //getting first digit 
+        int mysteryLength = stringMystery.length() - 1; //getting file length
+        int lastDigit = Integer.parseInt(stringMystery.substring(mysteryLength, mysteryLength + 1)); //getting last digit
+        int secondDigit = 0, count = 0; //initializing varables
         
-        count = 0;
+        
+        if (currentDigit == lastDigit){ //checking if first digit is equal to last digit in input 
+                count += currentDigit;
 
-        mysteryLength = stringMystery.length() - 1;
-
-
-
-        for (int i = 0; i < mysteryLength; i++) {
-            currentDigit = Integer.parseInt(stringMystery.substring(i, i+1));
-            secondDigit = Integer.parseInt(stringMystery.substring(i+1, i+2));
-
-
-            if (currentDigit == secondDigit) {
-                count += currentDigit - '0';
-            }
-            if (currentDigit == lastDigit){
-                count += currentDigit - '0';
-
-            }
         }
-        System.out.print("Count: "+count);
+        for (int i = 0; i < mysteryLength; i++) { //loop executes the same time as charachters in input
+            currentDigit = Integer.parseInt(stringMystery.substring(i, i+1)); //gathering first number in string 
+            secondDigit = Integer.parseInt(stringMystery.substring(i+1, i+2)); //gathering second number in string
+
+
+            if (currentDigit == secondDigit) { //comparing first number to second and adding if equal 
+                count += currentDigit;
+             }
+        }
+        System.out.println("Part 1 count: "+count); //output
         
         
         mysteryLength = (stringMystery.length() - 1)/2;
+        
         
         for (int i = 0; i < mysteryLength; i++){
             
@@ -53,7 +52,8 @@ public class StringMystery {
             }
             
         }
-        System.out.print("Count: "+count);
+        System.out.print("Part 2 count: "+count);
+        
         }   
-}
+}       
 
